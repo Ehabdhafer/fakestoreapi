241afea34dea104dcc82fca5a1153ce7d083492e
Ehabdhafer <ehabzaferabed@gmail.com>
2023-10-10 00:08:06 +0300

    add json files

--- app.js
+++ app.js
@@ -28,3 +28,46 @@ async function render() {
   });
 }
 render();
+
+// -----------------------------------------------------------------------------
+
+let array2 = [];
+let Sub = document.querySelector("#submit");
+Sub.addEventListener("click", (e) => {
+  e.preventDefault();
+  let name = document.getElementById("Name").value;
+  let age = document.getElementById("Age").value;
+  fetch("http://localhost:3000/posts", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({
+      id: "",
+      name: `${name}`,
+      age: `${age}`,
+    }),
+  });
+});
+
+const card3 = document.getElementById("div4");
+
+fetch("http://localhost:3000/posts")
+  .then((res) => res.json())
+  .then((data2) => {
+    data2.forEach((post) => {
+      const card = document.createElement("div");
+      card.classList.add("card");
+
+      const name = document.createElement("h3");
+      name.textContent = post.name;
+
+      const age = document.createElement("p");
+      age.textContent = post.age;
+
+      card.appendChild(name);
+      card.appendChild(age);
+
+      card3.appendChild(card);
+    });
+  });
